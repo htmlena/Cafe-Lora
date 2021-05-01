8e4975cfd71130c875619648c75f4d3b6bb5757e
Lenka Shynkarova <o.shynkarova@gmail.com>
2021-05-01 14:10:27 +0200

    Cycle for Layer component created

--- src/index.js
+++ src/index.js
@@ -41,6 +41,21 @@ orderBtnElm.addEventListener('click', () => {
 
 const drinkInfoElm = document.querySelector('.drink__info');
 
-drinkInfoElm.innerHTML += Layer({ color: '#feeeca', label: 'mléčná pěna' });
-drinkInfoElm.innerHTML += Layer({ color: '#fed7b0', label: 'teplé mléko' });
-drinkInfoElm.innerHTML += Layer({ color: '#613916', label: 'espresso' });
+const layers = [
+  {
+    color: '#feeeca',
+    label: 'mléčná pěna',
+  },
+  {
+    color: '#fed7b0',
+    label: 'teplé mléko',
+  },
+  {
+    color: '#613916',
+    label: 'espresso',
+  },
+];
+
+for (let i = 0; layers.length; i++) {
+  drinkInfoElm.innerHTML += Layer(layers[i]);
+}
