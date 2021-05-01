280da89334db45763222f3f9a54a2cbdc67a2630
Lenka Shynkarova <o.shynkarova@gmail.com>
2021-05-01 11:37:09 +0200

    Hamburger menu links to disappear on click

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import './style.css';
 
-console.log('funguju!');
+const navBtnElm = document.querySelector('#nav-btn');
+const navElm = document.querySelector('nav');
+navBtnElm.addEventListener('click', () => {
+  navElm.classList.toggle('nav-closed');
+});
+
+const navElmAll = document.querySelectorAll('.nav-link');
+
+for (let i = 0; i < navElmAll.length; i++) {
+  navElmAll[i].addEventListener('click', () => {
+    navElm.classList.add('nav-closed');
+  });
+}
