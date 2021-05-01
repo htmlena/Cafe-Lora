e4e25fb661182534a4c38a62a4de4aae5e40bb3a
Lenka Shynkarova <o.shynkarova@gmail.com>
2021-05-01 15:29:30 +0200

    Button functionality added into the Drink component

--- src/Drinks/index.js
+++ src/Drinks/index.js
@@ -21,5 +21,25 @@ export const Drink = ({ id, name, ordered, layers }) => {
             <div class="drink__controls">
               <button class="order-btn">Objednat</button>
             </div>`;
+
+  // ordered button functionality
+
+  const orderBtnElm = drink.querySelector('.order-btn');
+  const drinkCupElm = drink.querySelector('.drink__cup');
+  orderBtnElm.addEventListener('click', () => {
+    orderBtnElm.textContent = 'Zrušit';
+    drinkCupElm.classList.add('drink__cup--selected');
+
+    if (ordered === true) {
+      orderBtnElm.textContent = 'Objednat';
+      drinkCupElm.classList.remove('drink__cup--selected');
+      ordered = false;
+    } else if (ordered === false) {
+      orderBtnElm.textContent = 'Zrušit';
+      drinkCupElm.classList.add('drink__cup--selected');
+      ordered = true;
+    }
+  });
+
   return drink;
 };
